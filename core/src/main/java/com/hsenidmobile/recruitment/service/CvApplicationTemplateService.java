49c624d5b7224692c7eaaae2e322fe51ae299c3c
darshanac <darshanac@hsenidmobile.com>
2013-06-17 10:48:53 +0530

    CvApplicationTemplateService added

---
+++ core/src/main/java/com/hsenidmobile/recruitment/service/CvApplicationTemplateService.java
@@ -0,0 +1,40 @@
+package com.hsenidmobile.recruitment.service;
+
+import com.hsenidmobile.recruitment.model.CvApplicationTemplate;
+
+public interface CvApplicationTemplateService {
+
+    /**
+     * <p>
+     *     create application template
+     * </p>
+     * @param cvApplicationTemplate as {@link CvApplicationTemplate}
+     */
+    void create(CvApplicationTemplate cvApplicationTemplate);
+
+    /**
+     * <p>
+     *     update cv template
+     * </p>
+     * @param cvApplicationTemplate as {@link CvApplicationTemplate}
+     */
+    void update(CvApplicationTemplate cvApplicationTemplate);
+
+    /**
+     * <p>
+     *     remove the given cv template
+     * </p>
+     * @param cvApplicationTemplate as {@link CvApplicationTemplate}
+     */
+    void removeCvTemplate(CvApplicationTemplate cvApplicationTemplate);
+
+
+    /**
+     * <p>
+     *     find cv template with given id
+     * </p>
+     * @param id as {@link String}
+     * @return instance of {@link CvApplicationTemplate}
+     */
+    CvApplicationTemplate findCvTemplateById(String id);
+}
